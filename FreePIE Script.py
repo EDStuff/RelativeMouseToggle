887cccacf2407c885c912c51c6ab078a2bb26ed0
Andrea Spada <andreaspada@users.noreply.github.com>
2018-03-06 09:35:11 -0300

    Updated
    Ver 3.4 - commented unneeded code

--- FreePIE Script.py
+++ FreePIE Script.py
@@ -1,13 +1,13 @@
-# Title: Trackball/Mouse to Analog Axis for Elite: Dangerous, with "Relative Mouse" additional axis.
+# Title: Trackball/Mouse to Analog Axis for Elite: Dangerous, with "Relative Mouse" additional axis emulation.
 # Author: Andrea Spada
-# Version: 3.3
+# Version: 3.4
 #
 # Features: Simulate analog axis from mouse for yaw and pitch. It use vJoy.
 #
 # Each mouse direction is mapped to two axis. So, for lateral movement, we have both X and RX axis.
 # Vertical movements are mapped to Y and RY.
 #
-# X and Y give absolute mouse movement, like an analog joystick. They are smart auto-centering when near the center.
+# X and Y give absolute mouse movement, like an analog joystick with no springs. They are smart auto-centering when near the center.
 # The range of this self-centering (mostly for aim purpouse) is defined by a customizable radius. 
 # They also has a small exponential curve, so near zaro they give a smooth movement. The farther, the coarser.
 #
@@ -17,8 +17,6 @@
 # Both movement can be easily tweaked in sensitivity.
 #
 # Two more axis are used for the SRV, configured differently: horizontal movement is steeering, vertical movement is throttle.
-#
-# Joystick is maped to a virtual XBox controller, through ScpDriver and XOutput plugin.
 
 from System import Int16
 
@@ -59,26 +57,27 @@ if starting:
 
 # Global parameters
 global absolute_sens, relative_sens, steering_sens, srvthrottle_sens, relative_range, smart_speed, rel_speed, curve, mradius, tradius, mcurve, mratio, jcurve, jratio, scurve, sratio
-absolute_sens = 45		    # absolute mouse mode sensitivity
-relative_sens = 100	    	# relative mouse mode sensitivity
+
+absolute_sens = 60		    	# absolute mouse mode sensitivity
+relative_sens = 85	    		# relative mouse mode sensitivity
 steering_sens = 70			# steering sensitivity
-srvthrottle_sens = 20		# SRV throttle sensitivity
+srvthrottle_sens = 18			# SRV throttle sensitivity
 
-relative_range = 450		# relative mouse range for auto centering
-smart_speed = 25 		    # smart-centering speed, in absolute mouse mode
-rel_speed = 500		    	# hard-centering speed, in relative mouse mode
+relative_range = 450			# relative mouse range for auto centering
+smart_speed = 25 			# smart-centering speed, in absolute mouse mode
+rel_speed = 500		    		# hard-centering speed, in relative mouse mode
 
-mcurve = 1.35  			    		# exponential factor for the mouse axis curve
+mcurve = 1.35  			    	# exponential factor for the mouse axis curve
 mratio = (max ** mcurve ) / max 	# ratio to normalize mouse range
-jcurve = 2.0						# exponential factor for the joystick axis
+jcurve = 2.0				# exponential factor for the joystick axis
 jratio = (max ** jcurve ) / max		# ratio to normalize joystick range
-scurve = 1.6						# exponential factor for the steering axis
+scurve = 1.6				# exponential factor for the steering axis
 sratio = (max ** scurve ) / max		# ratio to normalize steering range
 
-mradius = 4000                      # smart self-centering radius, for absolute mouse
-tradius = 750						# SRV Throttle auto centering for easy halt
+mradius = 3000                      	# smart self-centering radius, for absolute mouse
+tradius = 750				# SRV Throttle auto centering for easy halt
 throttle_speed = 25
-srvthrottle_inc = 250				# SRV Throttle increment multiplicator
+srvthrottle_inc = 250			# SRV Throttle increment multiplicator
 
 #
 ###
@@ -214,7 +213,6 @@ if (srvthrottle > (tradius * -1)) and (srvthrottle < 0):
 srvthrottle -= (mouse.wheelUp * srvthrottle_inc) 
 srvthrottle += (mouse.wheelDown * srvthrottle_inc) 
 
-
 # SRV Throttle Output
 vJoy[0].dial = filters.deadband(srvthrottle, 100)
 
@@ -224,26 +222,26 @@ vJoy[0].dial = filters.deadband(srvthrottle, 100)
 
 #
 ###
-##### Logitech G13
-joyX = joystick[0].x
-joyY = joystick[0].y
+##### Logitech G13 - UNCOMMENT IF NEED IT
+#joyX = joystick[0].x
+#joyY = joystick[0].y
 
 # Exponential curved axis
-if (joyX > 0):
-	joyXcurved = math.floor((joyX ** jcurve) / jratio) 
-if (joyX < 0):
-	joyXn = joyX * -1
-	joyXcurved = math.floor(((joyXn ** jcurve) * -1) / jratio)
-
-if (joyY > 0):
-	joyYcurved = math.floor((joyY ** jcurve) / jratio) 
-if (joyY < 0):
-	joyYn = joyY * -1
-	joyYcurved = math.floor(((joyYn ** jcurve) * -1) / jratio)
+#if (joyX > 0):
+#	joyXcurved = math.floor((joyX ** jcurve) / jratio) 
+#If (joyX < 0):
+#	joyXn = joyX * -1
+#	joyXcurved = math.floor(((joyXn ** jcurve) * -1) / jratio)
+#
+#if (joyY > 0):
+#	joyYcurved = math.floor((joyY ** jcurve) / jratio) 
+#if (joyY < 0):
+#	joyYn = joyY * -1
+#	joyYcurved = math.floor(((joyYn ** jcurve) * -1) / jratio)
 
 # Joystick output
-vJoy[0].z = filters.deadband(joyXcurved, 50)
-vJoy[0].rz = filters.deadband(joyYcurved, 50)
+#vJoy[0].z = filters.deadband(joyXcurved, 50)
+#Joy[0].rz = filters.deadband(joyYcurved, 50)
 
 #####
 ###
@@ -266,14 +264,14 @@ if keyboard.getKeyDown(Key.NumberPad8) and keyboard.getKeyDown(Key.RightShift):
 	vJoy[0].ry = max
 
 # Joystick Axis
-if keyboard.getKeyDown(Key.NumberPad4) and keyboard.getKeyDown(Key.LeftShift):
-	vJoy[0].z = min
-if keyboard.getKeyDown(Key.NumberPad2) and keyboard.getKeyDown(Key.LeftShift):
-	vJoy[0].rz = max
-if keyboard.getKeyDown(Key.NumberPad6) and keyboard.getKeyDown(Key.LeftShift):
-	vJoy[0].z = max 
-if keyboard.getKeyDown(Key.NumberPad8) and keyboard.getKeyDown(Key.LeftShift):
-	vJoy[0].rz = min
+#if keyboard.getKeyDown(Key.NumberPad4) and keyboard.getKeyDown(Key.LeftShift):
+#	vJoy[0].z = min
+#if keyboard.getKeyDown(Key.NumberPad2) and keyboard.getKeyDown(Key.LeftShift):
+#	vJoy[0].rz = max
+#if keyboard.getKeyDown(Key.NumberPad6) and keyboard.getKeyDown(Key.LeftShift):
+#	vJoy[0].z = max 
+#if keyboard.getKeyDown(Key.NumberPad8) and keyboard.getKeyDown(Key.LeftShift):
+#	vJoy[0].rz = min
 
 #####
 ###
@@ -281,7 +279,9 @@ if keyboard.getKeyDown(Key.NumberPad8) and keyboard.getKeyDown(Key.LeftShift):
 
 #
 ###
-##### Soft Centering - slow down axis values
+##### Utilities
+
+# Soft Centering - slow down axis values
 
 if keyboard.getKeyDown(Key.LeftControl):
 	if mouseX > 0:
@@ -299,10 +299,7 @@ if keyboard.getKeyDown(Key.LeftControl):
 	if steerX < 0:
 		steerX = steerX + 100
 	
-#
-###
-##### Hard Centering (By press an hotkey)
-
+# Hard Centering (By press an hotkey)
 # Useful when you need your mouse to return to the center, like when you switch workspaces or exiting galaxy map...
 
 if keyboard.getKeyDown(Key.LeftControl) and keyboard.getPressed(Key.LeftWindowsKey):
@@ -350,10 +347,5 @@ diagnostics.watch(vJoy[0].rx)
 diagnostics.watch(vJoy[0].ry)
 diagnostics.watch(vJoy[0].slider)
 diagnostics.watch(vJoy[0].dial)
-diagnostics.watch(vJoy[0].z)
-diagnostics.watch(vJoy[0].rz)
-
-# Toggles
-diagnostics.watch(roll2yaw)
-diagnostics.watch(vertical2pitch)
-diagnostics.watch(mouse2joystick)
+#diagnostics.watch(vJoy[0].z)
+#diagnostics.watch(vJoy[0].rz)
